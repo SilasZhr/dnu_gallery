8210fb1087103622fbe3727299d151204e0c9288
Su Tao <sutao@SudeMacBook-Pro.lan>
2022-01-11 22:08:58 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,53 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config();
+
+
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+task("deploy", "Deploy the smart contracts", async (taskArgs, hre) => {
+
+  const Artwork = await hre.ethers.getContractFactory("Artwork");
+  const artwork = await Artwork.deploy("Artwork Contract", "ART");
+
+  await artwork.deployed();
+
+  await hre.run("verify:verify", {
+    address: artwork.address,
+    constructorArguments: [
+      "Artwork Contract",
+      "ART"
+    ]
+  })
+
+})
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ETH_RINKEBY_PROVIDER,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_KEY,
+    }
+  }
+};
